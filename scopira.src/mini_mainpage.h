f1d02f7144d1fef596f6924b8192dc4f16d41dc7
Aleksander Demko <ademko@gmail.com>
2011-12-01 03:19:56 -0600

    added doxygen files, updated README

---
+++ scopira.src/mini_mainpage.h
@@ -0,0 +1,17 @@
+
+/**
+ \mainpage Scopira Documentation
+
+ \section docindexsec Table of Contents
+
+ Core manuals:
+  - \ref scopirasyspage
+
+ Utilities:
+  Scopira now uses CMake (http://cmake.org/)
+
+ Other manuals may also be available. See the "Related Pages"
+ for a full list.
+
+*/
+
